d4ad9df6514eb428733b4826419571f0af736675
dennis <ensurios@gmx.net>
2023-02-09 18:53:21 +0100

    "commit"

---
+++ Klassen.py
@@ -0,0 +1,20 @@
+class Hund:
+    sex = "blah"
+
+    
+    def function(self):
+        print("This is a message inside a class")
+
+
+Max = Hund()
+Max.function()
+
+
+Kalle
+Knut
+Max
+
+
+Maja
+Luna
+Emma
